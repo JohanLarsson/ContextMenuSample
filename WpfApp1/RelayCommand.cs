bfe50e178b0d1d793f8f26df5dd7ac4243e66081
JohanLarsson <johan_gavle1@hotmail.com>
2018-11-11 22:36:49 +0100

    Initial

---
+++ WpfApp1/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿namespace WpfApp1
+{
+    using System;
+    using System.Windows.Input;
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentException("execute");
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null ? true : this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute.Invoke(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ WpfApp1/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApp1
+{
+    using System.Collections.ObjectModel;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public class ViewModel
+    {
+        public ObservableCollection<Player> Players { get; } = new ObservableCollection<Player> { new Player() };
+
+        public ICommand MyCommand { get; } = new RelayCommand(_ => MessageBox.Show("someone clicked"));
+    }
+}
